0893049bfae3c6734647f986b775a3f70b9a7858
KasunViranga1 <kasunviranga12@gmail.com>
2024-12-15 22:50:37 +0530

    structuring nested files and exporting standards

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 // modules protect their variables and functions from leaking
 require("./hello.js");
 
-const { x, calculateSum } = require("./sum.js");
+const { x, calculateSum } = require("./calculate/sum.js");
+const { calculateMultiplication } = require("./calculate/multiply.js");
 
 const a = 10;
 const b = 20;
 
 calculateSum(a, b);
+
+calculateMultiplication(a, b);
 console.log(x);
